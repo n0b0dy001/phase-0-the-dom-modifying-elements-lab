1ae8547c378ac9647115f7d5d75c293b0a51f59c
Christian Reyna <christianreyna12@gmail.com>
2023-08-25 16:34:02 -0500

    insert DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+  mainNode.remove();
+}
